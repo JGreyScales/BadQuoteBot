f7e672768749e26d1f7eb4fbf7b686ff4d1c2ba8
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2022-07-18 02:23:33 -0400

    fixed this piece of shit

--- discordmain.py
+++ discordmain.py
@@ -1,56 +1,77 @@
-import discord, os, json, time
-from scripts import scriptClass
+import os, discord, random, asyncio
 
+from utils.Admin import admin
+from utils.DD import dnd as DD
+from utils.Fun import fun_functions as fun
+from utils.utilities import utilities as util
 
+#function
 client = discord.Client()
-sC = scriptClass()
-f = open('config.json')
-d = json.load(f)
-f.close()
-config = {
-    "Prefix":d["Prefix"]
+
+#str
+prefix = '$$'
+Tmessage = ''
+
+#array
+counter = []
+admins = [554486518543155212]
+statuses = ['Doing dumb shit', 'Listening to some busting tunes', 'LUST MONTH LUST MONTH LUST MONTH', 'Vibing', 
+'Single/Depressed/Bullied/Half Demon/Trans/Has A Huge Secret ;)/Quirky/Mysterious/Gay/straight', 'Visual Studio Code', 'Adding a backend...',
+'Stealing server info', 'Pampcakes', 'hehe, A-10 chan go BRRRRRRRRRRRRRRRRTTTTTTT', 'BRRRRRRRRRRRRRRRTTTTTT', 'with your mom', 'rolling a crit miss', 'rolling a max crit',
+'WE NEED A SAILOR']
+
+#dict
+functions = {
+    'message counter' : ( lambda: (util.message_counter_return(Tmessage, counter))),
+    'echo' : ( lambda: (fun.echo(Tmessage, prefix, Tmessage_content))),
+    'diceroll' : ( lambda: (fun.diceroll(Tmessage, prefix, Tmessage_content))),
+    'kick' : ( lambda: (admin.kick(Tmessage, admins))),
+    'ban' : ( lambda: admin.ban(Tmessage, admins)),
+    'mute': lambda: (admin.mute(Tmessage, admins)),
+    'd_diceroll': ( lambda: (DD.dddiceroll(Tmessage, prefix))),
+    'dd' : (lambda: admin.dnd(Tmessage))
 }
 
+
+async def status_changer(client, status):
+    await client.change_presence(activity=discord.Game(name=random.choice(status)))
+    await asyncio.sleep(300)
+    await status_changer(client, status)
 
 
 @client.event
 async def on_ready():
-    print('Bot is online')
+    print("Signed into: {0.user}".format(client))
+    await status_changer(client, statuses)
+
 
 @client.event
 async def on_message(message):
-    prefix = config["Prefix"]
+    global Tmessage, Tmessage_content, message_lowered
 
-    if message.content[:len(prefix)].lower() == prefix:
-        print(f'{message.author} used command {message.content[len(prefix):]}')
-        content = message.content[len(prefix):].lower()
-        if content[:6] == "prefix" and len(content[7:]) > 0:
-            config["Prefix"] = content[7:]
-            prefix = config["Prefix"]
-            f = open('config.json', 'w')
-            f.write(json.dump(config, f, indent=4))
-            f.close()
+    if not message.author.bot:
 
-        if content[:5] == "quote":
-            if len(content[5:]) > 0:
-                try:
-                    x = int(content[5:])
-                    for m in range(x):
-                        await message.channel.send(sC.generateQuote())
-                        time.sleep(.75)
-                except(ValueError):
-                    await message.channel.send("ValueError, Invalid input. Must be type [INT]")
+        if message.content.startswith(prefix) == False:
+            util.message_counter(message.guild.id, counter)
+    
+        else:
+            message_lowered = message.content.lower()
+            Tmessage_content = message.content.replace('@', '@​\u200b')
+            Tmessage = message
+    
+            util.message_counter(message.guild.id, counter)
+    
+            if message_lowered[len(prefix):] in functions:
+                await functions[message_lowered[len(prefix):]]()
+    
             else:
-                await message.channel.send(sC.generateQuote())
-
-        if content[:5] == 'image':
-            await message.channel.send(file=discord.File(sC.generateImage()))
-
-        if content[:6] == 'qimage':
-            img = sC.overlayQuote(sC.generateImage(), sC.generateQuote())
-            await message.channel.send(file=discord.File(img))
-            Sc.clearCache(img)
-
+                for key in functions:
+                    if message_lowered[len(prefix):].startswith(key):
+                        start_point = message_lowered.find(key) + len(key)
+                        function_str = message_lowered[len(prefix):start_point]
+                        await functions[function_str]()
+                        print(f'{function_str} has been run by {message.author} | {message.content}')
+                        break
 
 
 client.run(os.getenv("GreyBot"))
